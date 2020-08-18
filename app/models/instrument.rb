d4dfbebbfb788ba6de30dd0e11f827d35653ff05
Meagan Butters <meagan.butters@hotmail.com>
2020-08-18 18:02:53 -0300

    Form functionality done, ruby code in redirection needed

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -3,7 +3,7 @@ class Instrument < ApplicationRecord
   has_many :users
 
   validates :name, presence: true, uniqueness: true
-  validates :category, presence: true, inclusion: { in: %w(piano violin guitar drums) }
+  validates :category, presence: true, inclusion: { in: %w(Piano Violin Guitar Drums Flute Other) }
   validates :price, presence: true, numericality: { only_integer: true }
   validates :location, presence: true
 end
