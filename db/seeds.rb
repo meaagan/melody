61122b2e4998bda3f93a52be9e4c9823f1548b19
Millie Senecal <millie.senecal@gmail.com>
2020-08-18 11:59:52 -0400

    Add columns to bookings and instruments schemas, working on new and create actions for booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Instrument.create(name: "Super piano", category: "piano", location: "Montreal", price: 50)
