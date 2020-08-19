9950db9a76172da7b1c7c1a05ef60e012ddbc5d3
Millie Senecal <millie.senecal@gmail.com>
2020-08-19 12:12:10 -0400

    add seeds

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,9 +1,11 @@
 class Instrument < ApplicationRecord
-  has_many :bookings
-  has_many :users
+  has_many :bookings, dependent: :destroy
+  belongs_to :user
 
-  validates :name, presence: true, uniqueness: true
-  validates :category, presence: true, inclusion: { in: %w(Piano Violin Guitar Drums Flute Other) }
+  CATEGORY = %w(Piano Violin Guitar Drums Flute Other)
+
+  validates :name, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY }
   validates :price, presence: true, numericality: { only_integer: true }
   validates :location, :description, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Booking.destroy_all
+Instrument.destroy_all
 
+user1 = User.create!(email: "sarah@email.com", password: "123456", first_name: "Sarah", last_name: "Croche", address: "4520 Rue Boyer, Montréal, QC H2J 3E4")
+user2 = User.create!(email: "john@email.com", password: "123456", first_name: "John", last_name: "Doe", address: "4453 Ave Christophe-Colomb, Montreal, QC H2J 3G4")
+user3 = User.create!(email: "pierre@email.com", password: "123456", first_name: "Pierre", last_name: "Paul Jacques", address: "4363 Rue de la Roche, Montréal, QC H2J 3J2")
 
-Instrument.create(name: "Super piano", category: "piano", location: "Montreal", price: 50)
+100.times do
+  category = Instrument::CATEGORY.sample
+  Instrument.create!(
+    category: category,
+    name: "#{Faker::Color.color_name} #{category}",
+    price: Faker::Number.number(digits: 2),
+    location: ["Montreal", "Sherbrooke", "Quebec City", "Toronto", "Vancouver"].sample,
+    description: Faker::Lorem.paragraph,
+    user: [user1, user2, user3].sample
+    )
+end
+
+Booking.create!(user: user1, instrument: Instrument.first, content: "Please I want this instrument")
+Booking.create!(user: user1, instrument: Instrument.second, content: "Hello, I would like to rent your instrument")
+Booking.create!(user: user2, instrument: Instrument.second, content: "Hello, I would like to rent your instrument :)")
+Booking.create!(user: user2, instrument: Instrument.third, content: "Hi, I would like to rent your instrument")
+Booking.create!(user: user3, instrument: Instrument.fourth, content: "Hi, I would like to rent your instrument")
