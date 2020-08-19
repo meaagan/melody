a52ea34789c5860b78efd12fe21b4fe9a3be89af
Meagan Butters <meagan.butters@hotmail.com>
2020-08-19 14:55:48 -0300

    Changed schema, added geocoder gem, included geocoding in instruments

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -8,4 +8,7 @@ class Instrument < ApplicationRecord
   validates :category, presence: true, inclusion: { in: CATEGORY }
   validates :price, presence: true, numericality: { only_integer: true }
   validates :location, :description, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_152327) do
+ActiveRecord::Schema.define(version: 2020_08_19_174611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2020_08_19_152327) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.text "description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_instruments_on_user_id"
   end
 
