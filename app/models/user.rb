95a5652b38c86804f5c3b3a4b064d28d7fe58777
Meagan Butters <meagan.butters@hotmail.com>
2020-08-20 17:21:10 -0300

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
-  has_many :meetings, dependent: :destroy
   has_many :instruments, through: :bookings
   has_one_attached :photo
   # Include default devise modules. Others available are:
