53b4773d68dc5e50dcad4417087874598737a83d
Kyle Masterson <kylemasterson25@gmail.com>
2020-11-07 09:54:06 -0500

    api key fixes

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: ENV[U61U389K6T], api_key: ENV[97788b22777bc8cf34ccfba5d4000066] }
+AlgoliaSearch.configuration = { application_id: ENV['U61U389K6T'], api_key: ENV['97788b22777bc8cf34ccfba5d4000066'] }
